3e4a09567b7b4afaf9d91fda32ba253c89ba24d7
Martin Růžička <martinr@post.cz>
2021-11-14 20:35:05 +0100

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Martin Růžička <martinr@post.cz>
+ */
+$lang['langns']                = 'Jmenný prostor diskusních stránek';
+$lang['showbutton']            = 'Automaticky zobrazit tlačítko v kompatibilních šablonách?';
