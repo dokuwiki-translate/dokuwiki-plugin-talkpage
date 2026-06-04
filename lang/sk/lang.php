66b7ff51a6790d2e96b7acc935856c625be4c360
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:10:44 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['back']                  = 'Späť na stránku';
+$lang['add']                   = 'Začať diskusiu';
+$lang['talk']                  = 'Diskutovať o stránke';
